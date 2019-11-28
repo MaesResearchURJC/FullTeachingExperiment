d4d73d90c078a4104d9dc87ba4683b8d99aee11a
Maes95 <michel.maes95@gmail.com>
2019-11-28 14:17:57 +0100

    Add mvn image to test in E2E

--- runE2ETests.sh
+++ runE2ETests.sh
@@ -5,10 +5,10 @@ then
 fi
 
 TESTS=(
-    # 'UserRestTest'
+    'UserRestTest'
     'FullTeachingTestE2EChat' 
-    # 'FullTeachingTestE2EREST' 
-    # 'FullTeachingTestE2EVideoSession' 
+    'FullTeachingTestE2EREST' 
+    'FullTeachingTestE2EVideoSession' 
 )
 
 export BUG=$1
@@ -21,17 +21,20 @@ mkdir -p $BUG_PATH
 cd full-teaching-experiment
 git checkout -f demo
 git clean -fdx
+cp ../utils/maven-exec.sh maven-exec.sh
 cd ..
 
 echo "Starting FullTeaching using DockerCompose"
 
+# CREATE A DOCKER VOLUME TO STORE AND RE-USE THE MAVEN PACKAGES
+docker volume create --name maven-repo
 
 # INIT SERVER (SUT)
 docker-compose up -d
 
-EUS=$(docker inspect --format='{{ .NetworkSettings.Networks.elastest_elastest.IPAddress}}' fullteachingtest_eus_1)
+EUS=$(docker inspect --format='{{ .NetworkSettings.Networks.elastest_elastest.IPAddress}}' fullteachingexperiment_eus_1)
 export ET_EUS_API=http://$EUS:8040/eus/v1/
-export ET_SUT_HOST=$(docker inspect --format='{{ .NetworkSettings.Networks.elastest_elastest.IPAddress}}' fullteachingtest_full-teaching_1)
+export ET_SUT_HOST=$(docker inspect --format='{{ .NetworkSettings.Networks.elastest_elastest.IPAddress}}' fullteachingexperiment_full-teaching_1)
 
 # WAIT FOR SERVER STARTUP 
 while ! curl --insecure --silent --output /dev/null "https://$ET_SUT_HOST:$PORT" ; do
@@ -41,7 +44,7 @@ done
 
 echo "FullTeaching UP in $ET_SUT_HOST:$PORT"
 
-# WAIT FOR SERVER EUS
+# WAIT FOR EUS 
 while ! curl --insecure --silent --output /dev/null $ET_EUS_API ; do
    echo "-> EUS is not ready in address '$ET_EUS_API' - RETRY ..."
    sleep 10
@@ -49,29 +52,32 @@ done
 
 echo "EUS UP in $ET_EUS_API"
 
-
+echo "Waiting a bit more ... (120s)"
+sleep 120
 
 # RUN TESTs
 for TEST in ${TESTS[@]};
 do  
     echo "########## RUNNING TEST: $TEST"
     mkdir -p $BUG_PATH/$TEST/
-    mvn -B -Dtest=$TEST test -f $PROJECT/pom.xml -DbrowserVersion=74 &> $BUG_PATH/$TEST/$TEST-TEST.log
+    docker run --name $PROJECT-$TEST \
+        -v maven-repo:/root/.m2 \
+        -v $PWD/$PROJECT:/usr/$PROJECT \
+        -w /usr/$PROJECT \
+        -e ET_EUS_API=$ET_EUS_API\
+        -e ET_SUT_HOST=$ET_SUT_HOST\
+        -e TEST=$TEST \
+        --network="elastest_elastest" \
+        maven:3-jdk-8-slim \
+        ./maven-exec.sh 
+    # STORE LOGS AND REPORTS
+    docker logs $PROJECT-$TEST &> $BUG_PATH/$TEST/$TEST-TEST.log
     cp -r $PROJECT/target/surefire-reports/*$TEST.xml $BUG_PATH/$TEST/$TEST-surefire-report.xml
     docker-compose logs --no-color &> $BUG_PATH/$TEST/$TEST-SUT.log
+    # STOP AND REMOVE DOCKER CONTAINER
+    docker rm -f $PROJECT-$TEST    
 done
 
 
 # SHUTDOWN SERVER 
 docker-compose down
-
-# find . -type d -name UserRestTest -exec rm -rf {} \;
-# find . -type d -name FullTeachingTestE2EChat -exec rm -rf {} \;
-# find . -type d -name FullTeachingTestE2EREST -exec rm -rf {} \;
-# find . -type d -name FullTeachingTestE2EVideoSession -exec rm -rf {} \;
-# mvn -B -Dtest=$TEST test -DbrowserVersion=74
-
-# PASOS:
-# LANZAR EUS: docker run --rm --name eusmagico -e "ET_FILES_PATH_IN_HOST=/tmp" -e "ET_DATA_IN_HOST=/tmp" -p 8040:8040 -v /var/run/docker.sock:/var/run/docker.sock --network elastest_elastest -e "USE_TORM=true" elastest/eus
-# LEVANTAR APP: docker-compose up -f
-# CORRER TEST mvn -B -Dtest=$TEST test -DbrowserVersion=74
